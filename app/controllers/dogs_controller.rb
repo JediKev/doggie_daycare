c3e535d5786eedfa41b382ab712d31fdd41f2d80
Kevin Thorne <kevinthorne7@yahoo.com>
2016-04-25 10:05:14 -0500

    Moves form to partial

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -20,9 +20,17 @@ class DogsController < ApplicationController
     @dog = Dog.find(params[:id])
   end
 
+  def edit
+    @dog = Dog.find(params[:id])
+  end
+
+  def update
+    
+  end
+
 private
 	def dog_params
-		params.require(:dog) .permit(:name, :age, :breed)
+		params.require(:dog).permit(:name, :age, :breed)
 	end
 
 end
